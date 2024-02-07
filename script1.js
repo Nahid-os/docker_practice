389c6f1d6ebb74d6296d4af9330884411e8898b5
Md Nahid Husen Nayan <nayanm@roehampton.ac.uk>
2024-02-07 23:22:20 +0000

    Adding the script

---
+++ script1.js
@@ -0,0 +1,6 @@
+var name = "Md Nahid Husen Nayan";
+var age = 21;
+var teacher = false;
+document.write('Name ' + name + 'br');
+document.write('Age ' + age + '<br>');
+document.write('Is teacher? ' + teacher);
